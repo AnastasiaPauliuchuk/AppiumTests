313e8471591e570b8b0d271fb4a510d357c88f4a
Anastasiya_Pauliuchuk <anastasia_pauliuchuk@epam.com>
2018-03-26 10:17:22 +0300

    first commit

---
+++ src/test/java/tests/GmailTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import java.lang.annotation.Target;
+
+public class GmailTest extends BaseTest {
+
+
+    @Test
+    public void test() {
+        logger.info("start test");
+    }
+}
